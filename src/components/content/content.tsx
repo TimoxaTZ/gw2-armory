6f1289a869ff341393e1bd7753543ac69da44f8b
TimoxaTZ <t1m0xa067@gmail.com>
2021-09-03 15:38:08 +0300

    creating page design part 1

--- src/components/content/content.tsx
+++ src/components/content/content.tsx
@@ -2,24 +2,50 @@ import React from 'react';
 import styles from './content.module.css'
 
 export const Content = () => {
+
     return (
         <div className={styles.intro}>
             <div className={styles.inner}>
-                <h1 className={styles.title}>About GW2 Armory</h1>
+                <div>
+                    <h1 className={styles.title}>About GW2 Armory</h1>
+                </div>
+                <div>
+                    <a className={styles.button} href='#'>Learn more</a>
+                </div>
             </div>
             <div className={styles.inner}>
-                <a className={styles.button} href='#'>Learn more</a>
+                <div className={styles.slider} >
+                        <div className={styles.slider__item__active}>
+                            <span className={styles.slider__num}>01 </span>
+                            intro
+                        </div>
+                    <div className={styles.slider__item}>
+                        <span className={styles.slider__num}>02 </span>
+                        Work
+                    </div>
+
+                    <div className={styles.slider__item}>
+                        <span className={styles.slider__num}>03 </span>
+                        About
+                    </div>
+
+                    <div className={styles.slider__item}>
+                        <span className={styles.slider__num}>04 </span>
+                        Contacts
+                    </div>
+
+                </div>
             </div>
+            {/*<p className={styles.slider}>You need to use your personal API-key, which*/}
+            {/*    you can*/}
+            {/*    get in your GW2 account. Next step you need to log in using this API-key. After that you can*/}
+            {/*    use all*/}
+            {/*    armory tools and check your characters, achievements, inventory, storage, builds and current*/}
+            {/*    armor*/}
+            {/*    which equipped on selected character.*/}
+            {/*</p>*/}
 
-
-            <p>You need to use your personal API-key, which
-                you can
-                get in your GW2 account. Next step you need to log in using this API-key. After that you can
-                use all
-                armory tools and check your characters, achievements, inventory, storage, builds and current
-                armor
-                which equipped on selected character.
-            </p>
         </div>
+
     )
 }
