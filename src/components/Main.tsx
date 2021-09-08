2cf4bf2802c0842d7657469308a2bc9aece29915
TimoxaTZ <t1m0xa067@gmail.com>
2021-09-08 16:36:26 +0300

    some fixes on routing

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styles from './main.module.css'
 import {Header} from "./header/header";
 import {Content} from "./content/content";
@@ -8,9 +8,21 @@ import {Login} from "../features/Login/Login";
 import {Characters} from "../features/Characters/Characters";
 import {useAppSelector} from "../app/useAppSelector";
 import {Character} from "../features/Characters/Character/Character";
+import {useDispatch} from "react-redux";
+import {getCharactersTC} from "../features/Characters/characters-reducer";
+
+
 
 
 export const Main = () => {
+
+    const dispatch = useDispatch();
+
+
+
+    useEffect(() => {
+        dispatch(getCharactersTC("D49F610A-5D10-0D4C-986A-B7469B305227A200CF65-E569-44AD-8403-2DF8D5C960D1"));
+    }, [dispatch])
 
     const characters = useAppSelector(state => state.characters.characters)
 
@@ -24,9 +36,11 @@ export const Main = () => {
                     <Route exact path={'/'} render={() => <Content/>}/>
                     <Route exact path={'/login'} render={() => <Login/>}/>
                     <Route exact path={'/characters'} render={() => <Characters/>}/>
+
                     {characters.map(c => {
                         return <Route exact path={`/characters/${c.name}`} render={() => <Character name={c.name}/>}/>
                     })}
+
                     {/*<Navbar/>*/}
                     {/*<Footer/>*/}
                 </Container>
