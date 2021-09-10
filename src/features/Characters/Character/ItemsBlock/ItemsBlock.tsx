8461ad67e113646eba169e7f1fe71184507af1db
TimoxaTZ <t1m0xa067@gmail.com>
2021-09-10 13:58:48 +0300

    typization of ItemsBlock

--- src/features/Characters/Character/ItemsBlock/ItemsBlock.tsx
+++ src/features/Characters/Character/ItemsBlock/ItemsBlock.tsx
@@ -5,10 +5,46 @@ import styles from "./ItemsBlock.module.css"
 import {Grid} from "@material-ui/core";
 
 
+export type CharactersReducedType = {
+    [characterName: string]: CharacterType
+}
+
+export type EquipmentType = {
+        id: number,
+        slot: string,
+        upgrades: [],
+        skin: number,
+        binding: string,
+        bound_to: string
+}
+
+export type ItemsType = {
+    [itemName: string]: {
+        id: number,
+        slot: string,
+        infusions: [],
+        binding: string,
+        dyes: [],
+        skin: number,
+        stats: {}
+        upgrades: []
+    }
+}
+
+// export enum EquipmentSlots {
+//     Accessory1, Accessory2,  Amulet,
+//     Axe, Backpack, Boots, Coat, Gloves,
+//     Helm, HelmAquatic,  Leggings,  Pick,
+//     Ring1,  Ring2, Shoulders, Sickle,
+//     WeaponA1,  WeaponAquaticA,  WeaponAquaticB,
+//     WeaponB1,  WeaponB2,
+// }
+
+
 export const ItemsBlock = (props: CharacterType) => {
 
     const data:CharacterType[] = useAppSelector(state => state.characters.characters)
-    const characters:any = data.reduce((acc, character) => {
+    const characters:CharactersReducedType = data.reduce((acc, character) => {
         return {
             ...acc, [character.name]: {
                 ...character
@@ -16,13 +52,10 @@ export const ItemsBlock = (props: CharacterType) => {
         };
     }, {});
 
-    type CharacterType = typeof character;
-    type EquipmnetType = typeof equipment;
-    type ItemsType = typeof equipment;
-
     const character:any = characters[props.name]
-    const equipment = character['equipment']
-    const items = equipment.reduce((acc:any, item:any) => {
+    const equipment:EquipmentType[] = character['equipment']
+
+    const items:ItemsType = equipment.reduce((acc, item) => {
         return {
             ...acc, [item.slot]: {
                 ...item
