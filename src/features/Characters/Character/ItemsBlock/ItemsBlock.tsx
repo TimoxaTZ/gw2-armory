41b5463abef90063eed9f06e6d2e19cd1532913f
TimoxaTZ <t1m0xa067@gmail.com>
2021-09-10 13:11:35 +0300

    fixed ui bugs

--- src/features/Characters/Character/ItemsBlock/ItemsBlock.tsx
+++ src/features/Characters/Character/ItemsBlock/ItemsBlock.tsx
@@ -5,7 +5,6 @@ import styles from "./ItemsBlock.module.css"
 import {Grid} from "@material-ui/core";
 
 
-
 export const ItemsBlock = (props: CharacterType) => {
 
     const data:CharacterType[] = useAppSelector(state => state.characters.characters)
@@ -17,16 +16,45 @@ export const ItemsBlock = (props: CharacterType) => {
         };
     }, {});
 
-    const character = characters[props.name]
-    const equipment = character['equipment']
+    type CharacterType = typeof character;
+    type EquipmnetType = typeof equipment;
+    type ItemsType = typeof equipment;
 
-    const items:any = equipment.reduce((acc:any, item:any) => {
+    const character:any = characters[props.name]
+    const equipment = character['equipment']
+    const items = equipment.reduce((acc:any, item:any) => {
         return {
             ...acc, [item.slot]: {
                 ...item
             }
         };
     }, {});
+
+    const getItems = equipment.map((item:any) => {
+        if (items.hasOwnProperty(item.slot))
+            return (
+                <div className={styles.textfield}>
+                        {items[item.slot].slot}: {items[item.slot].id}
+                </div>
+            )
+    })
+
+    return (
+        <div className={styles.intro}>
+            <Grid container
+                  direction="row"
+                  item xs zeroMinWidth
+                  justifyContent="center"
+                  alignItems="center">
+
+                {getItems}
+
+            </Grid>
+        </div>
+
+    )
+
+
 
     // const getItem = (item: string) => {
     //    if (items.hasOwnProperty(item)) {
@@ -40,34 +68,4 @@ export const ItemsBlock = (props: CharacterType) => {
     //        )
     //    }
     // }
-
-    const getItems = equipment.map((item:any) => {
-        if (items.hasOwnProperty(item.slot))
-        return (
-            <Grid container
-                  direction="row"
-                  item xs zeroMinWidth
-                  justifyContent="center"
-                  alignItems="center">
-                <div>
-                        <div className={styles.textfield}>
-                            {items[item.slot].slot}: {items[item.slot].id}
-                        </div>
-                </div>
-            </Grid>
-
-        )
-    })
-
-    return (
-        <div className={styles.intro}>
-            {getItems}
-        </div>
-    )
 }
-
-// {helm && shoulders && coat && gloves && leggins && boots
-// && backpack && trinket1 && trinket2 && amulet && ring1 && ring2
-// && weaponA1 && weaponA2 && weaponB1 && weaponB2
-// && helmAquatic && weaponAquaticA && weaponAquaticB
-// && sickle && axe && pick !== undefined? <DIV> : <div className={styles.textfield}>CHARACTER HAS NO ITEMS!</div> }
